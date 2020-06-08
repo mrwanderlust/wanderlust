e6fca0495f3ae6c3834d2e9a154609b5042a9522
Jordan Kaye <jordan.kaye2@gmail.com>
2020-06-07 20:56:35 -0600

    Basic landing page template
    Bootstrap integration is working properly.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use warp::Filter;
 #[template(path = "hello.html")]
 struct Hello {
     name: String,
+    current: String,
+}
+
+#[derive(Template)]
+#[template(path = "home.html")]
+struct Home {
+    current: String,
 }
 
 #[tokio::main]
@@ -13,12 +20,19 @@ async fn main() {
     let hello = warp::path!("hello" / String).map(|name: String| {
         let t = Hello {
             name: name.to_string(),
+            current: "Home".to_string(),
+        };
+        warp::reply::html(t.render().unwrap())
+    });
+    let home = warp::path!("home").map(|| {
+        let t = Home {
+            current: "Home".to_string(),
         };
         warp::reply::html(t.render().unwrap())
     });
     let static_dir = warp::path("static").and(warp::fs::dir("src/static"));
 
-    warp::serve(hello.or(static_dir))
+    warp::serve(hello.or(static_dir).or(home))
         .run(([0, 0, 0, 0], 3030))
         .await;
 }
